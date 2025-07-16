4b15071e8ebe5fb998a24b5d6d1fb43ef417af81
AntuFTW <antarikshpatel2002@gmail.com>
2025-07-16 01:51:11 +0100

    dim_book is not SCD 2 table with tests

--- dbt_transform/models/marts/dim_book.sql
+++ dbt_transform/models/marts/dim_book.sql
@@ -1,19 +1,22 @@
 with unique_books as (
-select
-    distinct pa.ISBN as DIM_BOOK_ISBN,
-    TITLE as DIM_BOOK_TITLE,
-    PUBLISHER as DIM_BOOK_PUBLISHER,
-    IMPRINT as DIM_BOOK_IMPRINT,
-    PUBLICATION_DATE as DIM_BOOK_PUBLICATION_DATE,
-    PRODUCT_GROUP as DIM_BOOK_PRODUCT_GROUP,
-    DEPARTMENT as DIM_BOOK_DEPARTMENT,
-    SUB_DEPARTMENT as DIM_BOOK_SUB_DEPARTMENT,
-    CLASS as DIM_BOOK_CLASS
-from
-    {{ ref('int_store_sales__single_author_rows') }} as pa
+    select
+        distinct pa.ISBN as DIM_BOOK_ISBN,
+        TITLE as DIM_BOOK_TITLE,
+        PUBLISHER as DIM_BOOK_PUBLISHER,
+        IMPRINT as DIM_BOOK_IMPRINT,
+        PUBLICATION_DATE as DIM_BOOK_PUBLICATION_DATE,
+        PRODUCT_GROUP as DIM_BOOK_PRODUCT_GROUP,
+        DEPARTMENT as DIM_BOOK_DEPARTMENT,
+        SUB_DEPARTMENT as DIM_BOOK_SUB_DEPARTMENT,
+        CLASS as DIM_BOOK_CLASS,
+        TO_DATE(CREATED_AT) as DIM_BOOK_RRP_VALID_FROM
+    from
+        {{ ref('int_store_sales__single_author_rows') }} as pa
 )
 select
     row_number() over (order by DIM_BOOK_ISBN) as DIM_BOOK_ID,
-    *
+    *,
+    null::DATE as DIM_BOOK_RRP_VALID_TO,
+    True as DIM_BOOK_RRP_CURRENTLY_VALID
 from
-    unique_books
+    unique_books 
